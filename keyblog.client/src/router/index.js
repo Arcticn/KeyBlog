dcd97e54eb2699402aab31496bf0a62d088edb52
Yuankai Tang <t1581208805@outlook.com>
2024-05-20 21:59:26 +0800

    正文部分初步结束，最终使用md-editor-v3

--- keyblog.client/src/router/index.js
+++ keyblog.client/src/router/index.js
@@ -1,13 +1,13 @@
 import { createWebHistory, createRouter } from 'vue-router'
 import BlogList from '@/components/BlogList.vue'
 import BlogList_Next from '@/components/BlogList.Next.vue'
-import Article from '@/components/Article.vue'
+import ArticlePage from '@/components/ArticlePage.vue'
 
 
 const routes = [
     { path: '/', component: BlogList },
     { path: '/next', component: BlogList_Next },
-    {path:'/arcticle/:id', component: Article},
+    { path: '/article', component: ArticlePage },
 ]
 
 const router = createRouter({
